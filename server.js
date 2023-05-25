b7f5307a1de8b68397453be9dd9f66836d3fc85c
Govardhan0235 <govardhan0900@gmail.com>
2023-05-25 14:13:40 +0530

    second  commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is d
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to NodeJs Api Call." });
 });
 
 require("./app/routes/tutorial.routes.js")(app);
